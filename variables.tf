5c4359f89df45fb77cd7b94b41891e94ce70e481
Le Moment IT <le_moment_it@proton.me>
2023-04-01 09:53:47 +0700

    fix: add a variable to define the availability zone

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "size" {
   description = "Size of the volume"
   default     = 20
 }
+
+variable "availability_zone" {
+  type        = string
+  description = "availability zone to deploy the ebs"
+  default     = "us-west-2a"
+}
